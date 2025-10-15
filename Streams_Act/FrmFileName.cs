a90bf8d853aa0eb2882fcbb7f294c996d76f8450
janelfrancisco <janelfrancisco2122@gmail.com>
2025-10-15 19:03:26 +0800

    Set the value of the static string SetFileName

--- Streams_Act/FrmFileName.cs
+++ Streams_Act/FrmFileName.cs
@@ -12,14 +12,23 @@ namespace Streams_Act
 {
     public partial class FrmFileName : Form
     {
+        public static string SetFileName;
         public FrmFileName()
         {
             InitializeComponent();
+
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // 4-5.  Inside the event, click the Okay button. Set the value of the static string SetFileName by calling the
+            // value of txtFileName, and concatenate it with ".txt".
 
+            SetFileName = txtFileName.Text + ".txt";
+
+            // Call the Close() method afterward
+            this.Close();
         }
     }
 }
